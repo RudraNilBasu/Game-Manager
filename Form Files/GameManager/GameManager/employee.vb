9b926dce80d10ad8e6df82d4a2c28c5ea9641b09
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-05-29 23:59:16 +0530

    Search Employee

--- Form Files/GameManager/GameManager/employee.vb
+++ Form Files/GameManager/GameManager/employee.vb
@@ -1,5 +1,5 @@
 ﻿Public Class employee
-
+    Dim searchItem As String
     Private Sub Employee_DetailsBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles Employee_DetailsBindingNavigatorSaveItem.Click
         Me.Validate()
         Me.Employee_DetailsBindingSource.EndEdit()
@@ -10,6 +10,15 @@
     Private Sub employee_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'DatabaseDataSet.Employee_Details' table. You can move, or remove it, as needed.
         Me.Employee_DetailsTableAdapter.Fill(Me.DatabaseDataSet.Employee_Details)
+
+        ' After this from search tutorial
+        With DataGridView1
+            .ClearSelection()
+            .ReadOnly = True
+            .MultiSelect = False
+        End With
+        
+        ' search tut ends here
 
     End Sub
 
@@ -34,11 +43,69 @@
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
         If ComboBox1.SelectedItem = "All" Then
             DataGridView1.DataSource = Employee_DetailsBindingSource
+            searchItem = "All"
         ElseIf ComboBox1.SelectedItem = "Graphics" Then
-            DataGridView1.DataSource = Nothing
+            'DataGridView1.DataSource = Nothing
+            searchItem = "Graphics"
+        ElseIf ComboBox1.SelectedItem = "Programmer" Then
+            'DataGridView1.DataSource = Nothing
+            searchItem = "Programmer"
+        ElseIf ComboBox1.SelectedItem = "Sound" Then
+            'DataGridView1.DataSource = Nothing
+            searchItem = "Sound"
         End If
+
+        'Search tutorial begins here
+        If searchItem = "All" Then
+            On Error GoTo ErrRe
+            Employee_DetailsBindingSource.Filter = Nothing
+
+            With DataGridView1
+                .ClearSelection()
+                .ReadOnly = True
+                .MultiSelect = False
+                .DataSource = Employee_DetailsBindingSource
+            End With
+ErrEx:
+            Exit Sub
+ErrRe:
+            MsgBox("Error Occured Lol, didn't expect that, did you ? Err No " & Err.Number & vbNewLine & "Descripton : " & Err.Description)
+            Resume ErrEx
+        Else
+            On Error GoTo SearchErr
+
+            'Employee_DetailsBindingSource.Filter = "(Convert(ID, 'System.String') LIKE '" & _
+            '   searchItem & "')" & "OR (YourName LIKE '" & searchItem & "') OR LastName LIKE '" & searchItem & "')"
+            Employee_DetailsBindingSource.Filter = "(Convert(ID, 'System.String') LIKE '" & _
+               searchItem & "')" & "OR (NAME LIKE '" & searchItem & "') OR (Position LIKE '" & searchItem & "')"
+
+            If Employee_DetailsBindingSource.Count <> 0 Then
+                With DataGridView1
+                    .DataSource = Employee_DetailsBindingSource
+                End With
+            Else
+                MsgBox("lel, You don't have any employee in this position. Good Luck With your Project")
+                Employee_DetailsBindingSource.Filter = Nothing
+
+                With DataGridView1
+                    .ClearSelection()
+                    .ReadOnly = True
+                    .MultiSelect = False
+                    .DataSource = Employee_DetailsBindingSource
+                End With
+            End If
+
+SearchErr:
+            MsgBox("Error Occured Lol, didn't expect that, did you ? Err No " & Err.Number & vbNewLine & "Descripton : " & Err.Description)
+            Resume ErrEx
+
+
+        End If
+        ' Search tut ends here
+
     End Sub
 
     Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
@@ -52,4 +119,7 @@
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
         End
     End Sub
+
+
+
 End Class
--- Form Files/GameManager/GameManager/table.Designer.vb
+++ Form Files/GameManager/GameManager/table.Designer.vb
@@ -22,7 +22,70 @@ Partial Class table
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.Font = New System.Drawing.Font("Assassin$", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.AliceBlue
+        Me.Label1.Location = New System.Drawing.Point(299, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(217, 45)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Time Table"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(246, 138)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Monday"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(327, 138)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Tuesday"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(521, 138)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 17)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Thursday"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(415, 138)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 17)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Wednesday"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(615, 138)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(47, 17)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Friday"
         '
         'table
         '
@@ -30,9 +93,23 @@ Partial Class table
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ClientSize = New System.Drawing.Size(802, 457)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.SystemColors.AppWorkspace
         Me.Name = "table"
         Me.Text = "table"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
 End Class
